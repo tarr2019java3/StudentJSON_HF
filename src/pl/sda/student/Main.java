ada329a275adafc5ab214ccaad5a6ec30981d319
hannafuerst <fuerst13@gmail.com>
2020-02-29 11:50:12 +0100

    dodanie 5 studentow

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -9,11 +9,21 @@ public class Main {
 
     public static void main(String[] args) {
         Student student=new Student ("Jan", "Kowalski", 32 );
+        Student student1=new Student ("Zbychu", "Zieliński", 22 );
+        Student student2=new Student ("Iwo", "Wisniewski", 31 );
+        Student student3=new Student ("Krzyś", "Nowak", 34 );
+        Student student4=new Student ("Adam", "Kowal", 30 );
+        Student student5=new Student ("Piotr", "Gmoch", 33 );
 	// serializacji- przekształcenie obiektu na formę tekstową
         // biblioteka Jackson faster xml
         ObjectMapper objectMapper=new ObjectMapper();
         try {
             objectMapper.writeValue(new File("student.json"), student);
+            objectMapper.writeValue(new File("student1.json"), student1);
+            objectMapper.writeValue(new File("student2.json"), student2);
+            objectMapper.writeValue(new File("student3.json"), student3);
+            objectMapper.writeValue(new File("student4.json"), student4);
+            objectMapper.writeValue(new File("student5.json"), student5);
         } catch (IOException e) {
             e.printStackTrace();
         }
