6409dd5ccb6830f6438ae9b6a64b5ba6fb02989b
hannafuerst <fuerst13@gmail.com>
2020-02-29 12:01:59 +0100

    dorobiona lista

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -16,7 +18,15 @@ public class Main {
         Student student5=new Student ("Piotr", "Gmoch", 33 );
 	// serializacji- przekształcenie obiektu na formę tekstową
         // biblioteka Jackson faster xml
+
         ObjectMapper objectMapper=new ObjectMapper();
+        List<Student> studentList=new ArrayList<>();
+        studentList.add(student);
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+        studentList.add(student5);
         try {
             objectMapper.writeValue(new File("student.json"), student);
             objectMapper.writeValue(new File("student1.json"), student1);
@@ -24,6 +34,7 @@ public class Main {
             objectMapper.writeValue(new File("student3.json"), student3);
             objectMapper.writeValue(new File("student4.json"), student4);
             objectMapper.writeValue(new File("student5.json"), student5);
+            objectMapper.writeValue(new File("studentList.json"), studentList);
         } catch (IOException e) {
             e.printStackTrace();
         }
